c95f730e59f687f131392094c14244c06aeab90d
linxTeng <linx@yunlai.cn>
2018-04-26 14:43:22 +0800

    调整模版目录

--- src/test/java/cn/yunlai/base/GeneratorMain.java
+++ src/test/java/cn/yunlai/base/GeneratorMain.java
@@ -9,7 +9,8 @@ public class GeneratorMain {
 	@Test
 	public void generateSingleTable() throws Exception {
 		GeneratorFacade g = new GeneratorFacade();
+		g.getGenerator().setTemplateRootDir("classpath:template");
 		// g.deleteOutRootDir(); // 删除生成器的输出目录 //
-		g.generateByTable("point_log", "template");
+		g.generateByTable("point_log");
 	}
 }
